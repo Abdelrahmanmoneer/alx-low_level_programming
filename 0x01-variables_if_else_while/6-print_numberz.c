f60933c5aec7e2b206c6451c9648ae6ac98b63f3
Abdelrahman <abdelrahmanmoneer9356@gmail.com>
2023-08-11 12:02:39 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+  * main - entry point
+  *
+  * Description: print single digit numbers
+  *
+  * Return: Always 0 (Succeaa)
+*/
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+
+	{
+		putchar(digit + '0');
+		digit++;
+	}
+	putchar('\n');
+
+	return (0);
+}
