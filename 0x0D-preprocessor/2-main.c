52b5819cc9599166a623cbf8eaed52fedacb07a1
Abdelrahman <abdelrahmanmoneer9356@gmail.com>
2023-09-11 12:42:50 -0700

    c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+  * main - function that prints the name of thr file followed by new line
+  *
+  * Return : 0 success
+*/
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
