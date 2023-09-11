7745c200c1dedf2406afacfd1d2b0a1ff3680c5a
Abdelrahman <abdelrahmanmoneer9356@gmail.com>
2023-09-11 15:07:18 -0700

    a

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+  * struct dog - the dog struct
+  * @name: name of the dog
+  * @age: age of the dog
+  * @owner: owner of the dog
+  *
+  * Description: a little dog in a big world
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* _DOG_H */
